b743ec8ec1e4027bbf3b5629274ef36a1d527c0f
LucaDalRi <euro2xd@gmail.com>
2023-01-31 16:50:05 +0100

    Bonus completato

--- js/scripts.js
+++ js/scripts.js
@@ -3,24 +3,34 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-
+      listIsReady: false,
       email: [],
-      
+
     }
   },
 
-  mounted() {
 
+
+  
+  mounted() {
+    
     for (i = 0 ; i < 10 ; i++) {
       
       axios
-        .get("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then((risposta) => {
-          this.email.push(risposta.data.response);
+      .get("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((risposta) => {
+        
+        this.email.push(risposta.data.response);
+
+        if (this.email.length == 10) {
+          this.listIsReady = true
+        }
+
       });
-
-    }
+      
+      
+    };
+    
+  },
   
-  }
-
 }).mount('#app')
